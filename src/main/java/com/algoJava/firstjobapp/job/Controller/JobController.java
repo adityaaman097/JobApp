839fc79c6caaaa15517ba56e7ea885603e7fad7e
Aditya <aditya.aman097@gmail.com>
2024-08-10 14:48:34 +0530

    Added Code for CRUD operations

--- src/main/java/com/algoJava/firstjobapp/job/Controller/JobController.java
+++ src/main/java/com/algoJava/firstjobapp/job/Controller/JobController.java
@@ -1,6 +1,8 @@
 package com.algoJava.firstjobapp.job.Controller;
 
 import com.algoJava.firstjobapp.job.Entity.Job;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -30,5 +32,35 @@ public class JobController {
     public String saveJobs(@RequestBody Job job){
         jobList.add(job);
         return "Job added successfully";
+    }
+
+    @PutMapping("/job/{id}")
+    public ResponseEntity<Job> updateJobById(@PathVariable int id, @RequestBody Job job){
+        Optional<Job> existingJobOpt = jobList.stream().filter(j-> j.getId() == id).findFirst();
+
+        if (existingJobOpt.isPresent()){
+                Job existingJob = existingJobOpt.get();
+                existingJob.setTitle(job.getTitle());
+                existingJob.setDesc(job.getDesc());
+                existingJob.setLocation(job.getLocation());
+                existingJob.setMinSalary(job.getMinSalary());
+                existingJob.setMaxSalary(job.getMaxSalary());
+                return new ResponseEntity<>(existingJob, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/job/{id}")
+    public ResponseEntity<Optional<Job>> deleteById(@PathVariable int id) {
+        Optional<Job> existingJobOpt = jobList.stream().filter(j -> j.getId() == id).findFirst();
+
+        if (existingJobOpt.isPresent()){
+                jobList.remove(existingJobOpt.get());
+                return new ResponseEntity<>(existingJobOpt, HttpStatus.NO_CONTENT);
+            }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
